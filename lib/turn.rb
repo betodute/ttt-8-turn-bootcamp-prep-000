0b9a52a61e5a3c1a4159f951fede0ca10c4773db
Beto Dute <betodute@gmail.com>
2019-05-06 21:49:14 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !(position_taken?(board, index))
+end
+
+def position_taken?(board, index)
+  !(board[index] == "" || board[index] == " ")
+end
+
+def move(board, input, value = "X")
+  board[input] = value
+  return board
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
